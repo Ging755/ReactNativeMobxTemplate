66f390d5aa4451ab6d787a2ba309efeb9074309a
Ging755 <denis.milic2@gmail.com>
2020-04-26 12:35:04 +0200

    no message

---
+++ react-native-template/navigation/navigator.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from '../screens/main/main'
+
+const Stack = createStackNavigator();
+
+class Navigator extends Component {
+    render() {
+        return (
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={Main}
+                    options={{ title: 'Welcome' }}
+                />
+            </Stack.Navigator>
+        );
+    }
+}
+
+export default Navigator;
